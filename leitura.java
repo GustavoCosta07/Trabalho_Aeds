2276a2628dd3e464573aff316a8cdf8877be1aa6
Davi Gomes <gdavi989@gmail.com>
2022-07-01 21:59:57 -0300

    Função relatório estoque

--- leitura.java
+++ leitura.java
@@ -2,24 +2,47 @@ import java.util.Scanner;
 import java.io.*;
 
 public class leitura {
-    public static void exibe(String[][]x){
-        System.out.println("  Tipo     Categoria  Estoque  Preço Código");
-        for(int i=0;i<x.length;i++){                      
-            for(int j=0;j<x[i].length;j++){
-                
-                System.out.print(x[i][j]+" | ");
-                if(j==4){System.out.println();}
+    public static int dias = 1;
+    // public static void fimDia(){
+
+    // dias++;
+    // }
+    public static void relatorioEstoque(String[][] x) throws Exception {
+        FileWriter pw = new FileWriter("relatorioEstoque.txt", true);
+        pw.write("\n"+"  Relatorio de estoque dia "+dias+"\n");
+        pw.write(" Tipo      Categoria  Estoque  " + "\n");
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length - 2; j++) {
+
+                pw.write(x[i][j] + " | ");
+                if (j == 2) {
+                    pw.write("\n");
+                }
+            }
+        }
+
+        pw.close();
+    }
+
+    public static void exibe(String[][] x) {
+        System.out.println("  Tipo     Categoria  Estoque  ");
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length - 2; j++) {
+
+                System.out.print(x[i][j] + " | ");
+                if (j == 2) {
+                    System.out.println();
+                }
             }
         }
     }
+
     public static void main(String[] args) {
         try {
             File file = new File("estocagem.txt");
             Scanner ler = new Scanner(file);
 
             String[][] produtos = new String[4][5];
-
-
 
             int contador = 0;
 
@@ -29,15 +52,16 @@ public class leitura {
 
                 for (int j = 0; j < valores.length; j++) {
                     produtos[contador][j] = valores[j];
-                  //  System.out.println(produtos[contador][j]);
+                    // System.out.println(produtos[contador][j]);
                 }
                 contador++;
             }
             ler.close();
             exibe(produtos);
+            relatorioEstoque(produtos);
         } catch (Exception e) {
             System.out.println("erro -" + e.getMessage());
         }
     }
 }
-//teste
+// teste
